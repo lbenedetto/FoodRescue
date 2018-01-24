665e154ea4cf836113d14762f6caf85d64d3b5f6
lbenedetto <larsbenedetto@gmail.com>
2018-01-24 15:03:47 -0800

    Fix typo

--- Android/FoodRescue/app/src/main/java/edu/ewu/team1/foodrescue/FeederActivity.java
+++ Android/FoodRescue/app/src/main/java/edu/ewu/team1/foodrescue/FeederActivity.java
@@ -8,6 +8,6 @@ public class FeederActivity  extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sso);
+        setContentView(R.layout.activity_feeder);
     }
 }
