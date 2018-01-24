afd43272810dbefe16bf51a7032717166b9a1162
lbenedetto <larsbenedetto@gmail.com>
2018-01-24 15:38:22 -0800

    Finished initial UI design of Eater and Feeder activities

--- Android/FoodRescue/app/src/main/java/edu/ewu/team1/foodrescue/EaterActivity.java
+++ Android/FoodRescue/app/src/main/java/edu/ewu/team1/foodrescue/EaterActivity.java
@@ -3,11 +3,11 @@ package edu.ewu.team1.foodrescue;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class FeederActivity extends AppCompatActivity {
+public class EaterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_feeder);
+        setContentView(R.layout.activity_eater);
     }
 }
