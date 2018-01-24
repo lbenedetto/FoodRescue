e5c9b49b801954ff2350358c98d5e08503982905
Benedetto <lbenedetto@ewu.edu>
2018-01-24 14:53:19 -0800

    initial commit

---
+++ Android/FoodRescue/app/src/main/java/edu/ewu/team1/foodrescue/FeederActivity.java
@@ -0,0 +1,13 @@
+package edu.ewu.team1.foodrescue;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class FeederActivity  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sso);
+    }
+}
---
+++ Android/FoodRescue/app/src/main/java/edu/ewu/team1/foodrescue/MapView.java
@@ -0,0 +1,20 @@
+package edu.ewu.team1.foodrescue;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+
+public class MapView extends android.support.v7.widget.AppCompatImageView {
+
+    public MapView(Context context) {
+        super(context);
+    }
+
+    public MapView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MapView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
