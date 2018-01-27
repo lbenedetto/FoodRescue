a0b662f452c57ff5e5535fe0020c5a8fc3b5f95e
larperdoodle <larsbenedetto@gmail.com>
2018-01-26 18:08:55 -0800

    Made a good deal of progress on prototype (UI stuff only, not yet started on functionality)

--- Android/FoodRescue/app/src/main/java/edu/ewu/team1/foodrescue/EaterActivity.java
+++ Android/FoodRescue/app/src/main/java/edu/ewu/team1/foodrescue/EaterActivity.java
@@ -1,7 +1,10 @@
 package edu.ewu.team1.foodrescue;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class EaterActivity extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class EaterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eater);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.switcher, menu);
+        menu.findItem(R.id.itemEater).setChecked(true);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.itemFeeder) {
+            startActivity(new Intent(this, FeederActivity.class));
+            return true;
+        }else if (id == R.id.itemEater){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- Android/FoodRescue/app/src/main/java/edu/ewu/team1/foodrescue/SSOActivity.java
+++ Android/FoodRescue/app/src/main/java/edu/ewu/team1/foodrescue/SSOActivity.java
@@ -1,7 +1,10 @@
 package edu.ewu.team1.foodrescue;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class SSOActivity extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class SSOActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sso);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.switcher, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.itemFeeder) {
+            startActivity(new Intent(this, FeederActivity.class));
+            return true;
+        }else if (id == R.id.itemEater){
+            startActivity(new Intent(this, EaterActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
