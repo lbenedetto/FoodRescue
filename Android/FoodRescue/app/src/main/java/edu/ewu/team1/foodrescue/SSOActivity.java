5d1e751c3be9cadef2796bc04b8d5290d2df2895
larperdoodle <larsbenedetto@gmail.com>
2018-02-10 14:54:53 -0800

    App can now receive messages and notifications from Firebase

--- Android/FoodRescue/app/src/main/java/edu/ewu/team1/foodrescue/SSOActivity.java
+++ Android/FoodRescue/app/src/main/java/edu/ewu/team1/foodrescue/SSOActivity.java
@@ -5,6 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 import edu.ewu.team1.foodrescue.eater.EaterActivity;
 import edu.ewu.team1.foodrescue.feeder.FeederActivity;
@@ -15,6 +19,7 @@ public class SSOActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sso);
+        checkPlayServices();
     }
 
     @Override
@@ -34,5 +39,25 @@ public class SSOActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        checkPlayServices();
+    }
+
+    private void checkPlayServices() {
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        int resultCode = apiAvailability.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (apiAvailability.isUserResolvableError(resultCode)) {
+                apiAvailability.getErrorDialog(this, resultCode, 2404).show();
+                apiAvailability.makeGooglePlayServicesAvailable(this);
+            } else {
+                Toast.makeText(this, "This device is not supported.", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        }
     }
 }
