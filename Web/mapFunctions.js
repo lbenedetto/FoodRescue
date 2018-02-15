f181e8bc4d0f049f6e7843c1bfc6e4d5892b4ae9
BrittniLudington <goombablood@yahoo.com>
2018-02-14 19:28:50 -0800

    Update mapFunctions.js

--- Web/mapFunctions.js
+++ Web/mapFunctions.js
@@ -1,14 +1,36 @@
+var prevMarker = null;
 
 function initMap()
+
 {
     var uluru = {lat: 47.491602, lng: -117.584417};
     var map = new google.maps.Map(document.getElementById('map'),{
-        zoom: 17,
+        zoom: 16,
         center: uluru
     });
 
-    var marker = new google.maps.Marker({
-        position: uluru,
-        map: map
+    map.addListener("rightclick",function(e)
+    {
+        placeMarker(e.latLng,map);
     });
+
 }
+
+
+function placeMarker(latLng, map)
+{
+    if(prevMarker != null)
+        prevMarker.setMap(null);
+    var curMarker = new google.maps.Marker(
+    {
+        position: latLng,
+        map: map
+    })
+    prevMarker = curMarker;
+
+    var lat = latLng.lat();
+    var lng = latLng.lng();
+    var str = "location is " + lat + " degrees latitude and " + lng + " degrees longitude.";
+
+    $("#makerLocation").val(str);
+};
