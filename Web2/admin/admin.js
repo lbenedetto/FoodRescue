2154ca1b62a6809fc5f2b52b920717c60caa2773
larperdoodle <larsbenedetto@gmail.com>
2018-05-09 14:25:17 -0700

    Added admin page

--- Web2/admin/admin.js
+++ Web2/admin/admin.js
@@ -1,1 +1,38 @@
+var TABLE = $('#table');
+var CLONE = $('#clone');
+var id;
 
+$.fn.exists = function () {
+	return this.length !== 0;
+};
+
+$.get("admin.php", handleResponse);
+
+function updateRow(id, data) {
+	var row = $("#" + id);
+	var exists = row.exists();
+	if (!exists) {
+		row = CLONE.find('tr.hide').clone(true).removeClass('hide table-line');
+		row.attr('id', id);
+	}
+	var col = row.find('td');
+	for (var i = 0; i < 5; i++) {
+		col[i + 1].innerText = data[i];
+	}
+	if (!exists) {//Add it to the table if it isn't already in the table
+		TABLE.find('table').append(row);
+	}
+}
+
+function search() {
+	var query = $("#searchBar").val();
+	$.get("admin.php/?search=" + query, handleResponse);
+}
+
+function handleResponse(response){
+	var re = JSON.parse(response);
+	for (var i = 0; i < re.length; i++) {
+		var r = re[i];
+		updateRow(r["id"], [r["name"], r["permission"]])
+	}
+}
---
+++ Web2/admin/admin.php
@@ -0,0 +1,4 @@
+<?php
+//TODO: Brad, the html page will do a get request to this file with a string to search for
+//If its blank, return the first 50 results
+//If its, for example, "a", return the first 50 results that start with a
