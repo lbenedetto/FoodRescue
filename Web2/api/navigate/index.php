e75eaa038beb07ee34ede668cd8d9b24996d88c4
BradStephenson <arbiteroforder@gmail.com>
2018-05-11 13:53:47 -0700

    Update index.php

--- Web2/api/navigate/index.php
+++ Web2/api/navigate/index.php
@@ -3,3 +3,39 @@
 //User is allowed to access based on their permission level
 //Admins get Admin, Feeder, and Eater pages
 //Eater gets only Eater page, ect
+if ($_SERVER['REQUEST_METHOD'] == 'GET') 
+{
+  $perm = 0;
+  if (isset($_GET['auth'])) 
+	{
+    $conn = getConn();
+    if ($conn) 
+    {
+      $stmt = $conn->prepare("SELECT * FROM users WHERE auth_token = ?");  // Look for uid
+      $stmt->bindValue(1, $_GET['auth'], PDO::PARAM_STR);
+      try 
+      {
+        $stmt->execute();
+      } catch (PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+      }
+      if ($stmt->rowCount() == 1) // Found the id
+      {
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        $perm = $row['feeder_perm'];         
+      }
+    }
+  }
+  
+  if ($perm > 0)
+    echo '<li><a href="../announce/"></li>\n';
+  if ($perm > 1)
+    echo '<li><a href="../admin/"></li>\n';
+    
+    
+  
+  
+}
+else
+  header("HTTP/1.0 405 MethodNotAllowed");
+?>
