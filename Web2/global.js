63df66d0bf7b6e361112b375b84ca4155c6bf5ff
larperdoodle <larsbenedetto@gmail.com>
2018-05-13 17:13:13 -0700

    Update app

--- Web2/global.js
+++ Web2/global.js
@@ -1,5 +1,5 @@
 var auth_token = $("#auth_token").val();
 
 function downloadApp() {
-	document.getElementById('iframeAppDownloader').src = "../assets/FoodRescue-18.5.9.apk";
+	document.getElementById('iframeAppDownloader').src = "../assets/FoodRescue-18.5.13.apk";
 }
