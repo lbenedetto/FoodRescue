7cd8e6bd0cd98b741b078baee333f7ffd3d0f45f
BradStephenson <arbiteroforder@gmail.com>
2018-05-15 09:52:31 -0700

    Update checkAuth.php
    now looks for auth_token in $_SESSION so links can be simpler

--- Web2/api/login/web/checkAuth.php
+++ Web2/api/login/web/checkAuth.php
@@ -1,8 +1,16 @@
 <?php
 include "../../auth_poster_functions.php";
-if (isset($_GET['auth_token'])) {
+if (isset($_GET['auth_token'])) 
+{
 	$auth_token = $_GET['auth_token'];
 	echo "<input id='auth_token' type='hidden' value='$auth_token'>";
-} else {
+} 
+else if (isset($_SESSION['auth_token'])) 
+{
+	$auth_token = $_SESSION['auth_token'];
+	echo "<input id='auth_token' type='hidden' value='$auth_token'>";
+}
+else 
+{
 	header("Location: https://login.ewu.edu/cas/login?service=".$_SESSION['SITE_URL']."api/login/web");
 }
