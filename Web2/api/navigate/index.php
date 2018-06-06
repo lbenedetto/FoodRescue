b561c1280c98128e65137e1335e55de1beec34ca
BradStephenson <arbiteroforder@gmail.com>
2018-06-06 00:07:42 -0700

    Update index.php

--- Web2/api/navigate/index.php
+++ Web2/api/navigate/index.php
@@ -1,4 +1,5 @@
 <?php
+include '../auth_poster_functions.php';
 //TODO: Respond to get request containing only an auth token with an array of <li> containing links to the pages this
 //User is allowed to access based on their permission level
 //Admins get Admin, Feeder, and Eater pages
